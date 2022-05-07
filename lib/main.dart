d4e673762c122f58ecaaf2bb2238ef37113d3eaf
nicecs15 <patcharapon.csit15@gmail.com>
2022-05-07 14:19:52 +0700

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:myapp1/notifier/north_notifier.dart';
 import 'package:myapp1/router.dart';
 
 String initialRoute = '/authen';
 
 Future<Null> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp().then((value) async {});
   await FirebaseAuth.instance.authStateChanges().listen((event) {
     if (event != null) {

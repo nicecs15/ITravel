bc263d56bb886da340cfef578262cbbdb7553b5f
deawtrp <105099839+deawtrp@users.noreply.github.com>
2022-05-08 17:12:01 +0700

    เดียวแก้อีสาน

--- lib/widget/isan_list.dart
+++ lib/widget/isan_list.dart
@@ -11,7 +11,7 @@ class _IsanListState extends State<IsanList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('this is isan'),
+      body: Text('หน้าอีสาน'),
     );
   }
 }
